0364e72632d3aab9a2bb93889b0f4ad965864407
Jose Rivera <josemartinriverasolis@gmail.com>
2024-07-25 17:19:32 -0600

    ultimo push

--- src/prueba/pkg1/Main.java
+++ src/prueba/pkg1/Main.java
@@ -4,10 +4,55 @@
  */
 package prueba.pkg1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author jmpin
  */
 public class Main {
     
+    static ArrayList<Object> list;
+    static Scanner lea;
+    
+    public Main() {
+        list = new ArrayList<>();
+        lea = new Scanner(System.in);
+    }
+    
+    public static void main(String[] args) {
+        
+        lea.useDelimiter("\n");
+        
+        System.out.print("Ingresar tipo: ");
+        String tipo = lea.next();
+        
+        if(tipo.equals("MOVIE")) {
+            
+            System.out.print("Ingresar codigo: ");
+            int codigo = lea.nextInt();
+            System.out.print("Ingresar nombre: ");
+            String nombre = lea.next();
+            System.out.print("Ingresar codigo: ");
+            double precio = lea.nextDouble();
+            
+            Movie movie = new Movie(codigo, nombre, precio);
+            list.add(movie);
+            
+        } else if(tipo.equals("PS3")) {
+            
+            System.out.print("Ingresar codigo: ");
+            int codigo = lea.nextInt();
+            System.out.print("Ingresar nombre: ");
+            String nombre = lea.next();
+            System.out.print("Ingresar codigo: ");
+            double precio = lea.nextDouble();
+            
+            PS3Game game= new PS3Game(codigo, nombre, precio);
+            list.add(game);
+            
+        } else {
+            System.out.println("Tipo Incorrecto");
+        }
+    }
 }
